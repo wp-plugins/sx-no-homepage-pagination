1b6421ac31daa75f1a2da9b15ca158806c252eee
Confridin <Confridin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-25 11:08:58 +0000

    Adding compatibily check
    git-svn-id: https://plugins.svn.wordpress.org/sx-no-homepage-pagination/trunk@864635 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sx-no-homepage-pagination.php
+++ sx-no-homepage-pagination.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name:  SX No Homepage Pagination
-Version:      1.0
+Version:      1.1
 Plugin URI:   http://www.seomix.fr
 Description:  SX No Homepage Pagination removes properly any homepage pagination (whatever plugin or function you are using) and redirect useless paginated content. This plugin works on any default homepage, not on a blog page.
 Availables languages : en_EN
-Tags: homepage, pagination
+Tags: homepage, pagination, page, paged, frontpage, home page, front page
 Author: Daniel Roch
 Author URI: http://www.seomix.fr
 Requires at least: 3.3
@@ -62,3 +62,56 @@ function seomix_redirect_homepage_pagination () {
   }
 }
 add_action( 'template_redirect', 'seomix_redirect_homepage_pagination' );
+
+/**
+ Check Compatibility
+ * http://wptavern.com/how-to-prevent-wordpress-plugins-from-activating-on-sites-with-incompatible-hosting-environments
+ */
+class seomix_nohomepage_checkcompatibility {
+    function __construct() {
+        add_action( 'admin_init', array( $this, 'check_version' ) );
+
+        // Don't run anything else in the plugin, if we're on an incompatible WordPress version
+        if ( ! self::compatible_version() ) {
+            return;
+        }
+    }
+
+    // The primary sanity check, automatically disable the plugin on activation if it doesn't
+    // meet minimum requirements.
+    static function activation_check() {
+        if ( ! self::compatible_version() ) {
+            deactivate_plugins( plugin_basename( __FILE__ ) );
+            wp_die('SX No homepage Pagination requires WordPress 3.3 or higher!');
+        }
+    }
+
+    // The backup sanity check, in case the plugin is activated in a weird way,
+    // or the versions change after activation.
+    function check_version() {
+        if ( ! self::compatible_version() ) {
+            if ( is_plugin_active( plugin_basename( __FILE__ ) ) ) {
+                deactivate_plugins( plugin_basename( __FILE__ ) );
+                add_action( 'admin_notices', array( $this, 'disabled_notice' ) );
+                if ( isset( $_GET['activate'] ) ) {
+                    unset( $_GET['activate'] );
+                }
+            }
+        }
+    }
+
+    function disabled_notice() {
+       echo '<div class="updated"><p><strong>SX No homepage Pagination requires WordPress 3.3 or higher!</strong></p></div>';
+    }
+
+    static function compatible_version() {
+        if ( version_compare( $GLOBALS['wp_version'], '3.3', '<' ) ) {
+             return false;
+         }
+        return true;
+    }
+}
+
+global $sxnohomepagecheck;
+$sxnohomepagecheck = new seomix_nohomepage_checkcompatibility();
+register_activation_hook( __FILE__, array( 'seomix_nohomepage_checkcompatibility', 'activation_check' ) );
